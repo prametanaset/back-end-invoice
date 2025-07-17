f4cf4c2133774b75b89134f84e0003185cae47b5
Bastaks14096 <taksin.k@kkumail.com>
2025-07-17 17:21:23 +0700

    update mail opt template

--- pkg/otp/email_template.go
+++ pkg/otp/email_template.go
@@ -8,55 +8,54 @@ func buildOTPEmail(to, code string) string {
 <html>
 <head>
   <meta charset="UTF-8">
-  <style>
-    body {
-      margin: 0;
-      padding: 0;
-      background-color: #f4f4f4;
-      font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
-    }
-    .container {
-      max-width: 600px;
-      margin: 40px auto;
-      background-color: #ffffff;
-      padding: 30px;
-      border-radius: 8px;
-      box-shadow: 0 4px 12px rgba(0, 0, 0, 0.1);
-    }
-    .header {
-      text-align: center;
-      padding-bottom: 20px;
-    }
-    .header h1 {
-      margin: 0;
-      font-size: 24px;
-      color: #af38ff;
-    }
-    .content {
-      text-align: center;
-      font-size: 16px;
-      color: #555555;
-    }
-    .otp-code {
-      margin: 20px 0;
-      font-size: 36px;
-      font-weight: bold;
-      color: #2b2b2b;
-      letter-spacing: 6px;
-      background-color:rgba(175, 56, 255, 0.22);
-      display: inline-block;
-      padding: 10px 20px;
-      border-radius: 6px;
-    }
-    .footer {
-      margin-top: 30px;
-      font-size: 13px;
-      color: #999999;
-      text-align: center;
-    }
-  </style>
 </head>
 <body>
+  <style>
+      body {
+        margin: 0;
+        padding: 0;
+        background-color: #f4f4f4;
+      }
+      .container {
+        max-width: 600px;
+        margin: 40px auto;
+        background-color: #ffffff;
+        padding: 30px;
+        border-radius: 8px;
+        box-shadow: 0 4px 12px rgba(0, 0, 0, 0.1);
+      }
+      .header {
+        text-align: center;
+        padding-bottom: 20px;
+      }
+      .header h1 {
+        margin: 0;
+        font-size: 24px;
+        color: #af38ff;
+      }
+      .content {
+        text-align: center;
+        font-size: 16px;
+        color: #555555;
+      }
+      .otp-code {
+        margin: 20px 0;
+        font-size: 36px;
+        font-weight: bold;
+        color: #2b2b2b;
+        letter-spacing: 6px;
+        background-color:rgba(175, 56, 255, 0.22);
+        display: inline-block;
+        padding: 10px 20px;
+        border-radius: 6px;
+      }
+      .footer {
+        margin-top: 30px;
+        font-size: 13px;
+        color: #999999;
+        text-align: center;
+      }
+  </style>
   <div class="container">
     <div class="header">
       <h1>รหัสยืนยัน (OTP)</h1>
